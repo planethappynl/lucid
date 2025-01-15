87db172d7905e5e3d1bfa51817a68a198efaa6a3
Lucas <l.bos@planethappy.nl>
2025-01-15 13:28:06 +0100

    Allow serving of feature objects

--- src/Bus/ServesFeatures.php
+++ src/Bus/ServesFeatures.php
@@ -5,6 +5,7 @@ namespace Lucid\Bus;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Support\Collection;
 use Lucid\Events\FeatureStarted;
+use Lucid\Units\Feature;
 
 trait ServesFeatures
 {
@@ -14,10 +15,16 @@ trait ServesFeatures
     /**
      * Serve the given feature with the given arguments.
      */
-    public function serve(string $feature, array $arguments = []): mixed
+    public function serve(string|Feature $feature, array $arguments = []): mixed
     {
-        event(new FeatureStarted($feature, $arguments));
+        if (!$feature instanceof Feature) {
+            event(new FeatureStarted($feature, $arguments));
 
-        return $this->dispatchSync($this->marshal($feature, new Collection(), $arguments));
+            return $this->dispatchSync($this->marshal($feature, new Collection(), $arguments));
+        }
+
+        event(new FeatureStarted($feature::class, $arguments));
+
+        return $this->dispatchSync($feature);
     }
 }
